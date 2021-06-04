b876749857a6ab203701c05fe91d698e50f4003c
Jin Watanabe <combatbutler0303@yahoo.co.jp>
2021-06-04 21:59:41 +0900

    210604

--- notebook/nishika/old_apartment_2021/pre3.py
+++ notebook/nishika/old_apartment_2021/pre3.py
@@ -72,7 +72,7 @@ def data_pre(df):
     area_df = pd.read_csv("./feature_data/axis_cluster.csv")
     df = pd.merge(df.reset_index(), area_df, on="市区町村名").set_index("ID")
 
-    for col in ["都道府県名", "市区町村名", "地区名", "最寄駅：名称", "間取り", "建物の構造", "用途", "今後の利用目的", "都市計画", "改装", "取引の事情等"]:
-        df[col] = df[col].astype("category")
+#     for col in ["都道府県名", "市区町村名", "地区名", "最寄駅：名称", "間取り", "建物の構造", "用途", "今後の利用目的", "都市計画", "改装", "取引の事情等"]:
+#         df[col] = df[col].astype("category")
  
     return df
